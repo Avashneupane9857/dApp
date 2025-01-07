f5fffeac0eb7876a4bcc5ae759b67e0554770d3a
Avashneupane9857 <juisch20220011252@gmail.com>
2025-01-07 22:15:37 +0530

    Public keys

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,6 +1,14 @@
+import { useWallet } from "@solana/wallet-adapter-react";
+
 export function Airdrop() {
+  const wallet = useWallet();
+  const publicKey = wallet?.publicKey?.toString();
+  if (!publicKey) {
+    return <p>loading.........</p>;
+  }
   return (
     <div className="relative top-10 text-black flex justify-center">
+      <h1>Hi Your public key is {publicKey}</h1>
       <input className="bg-slate-800" type="text" placeholder="amount" />
       <button className="bg-slate-400 text-black rounded-2xl w-32">
         Send airdrop
