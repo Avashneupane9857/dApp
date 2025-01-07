b0eeb94c193ad2c8aee66236a3d42717209de9f9
Avashneupane9857 <juisch20220011252@gmail.com>
2025-01-08 00:11:54 +0530

    feat: Airdrop featiures

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,16 +1,48 @@
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useState } from "react";
 
 export function Airdrop() {
   const wallet = useWallet();
+  const { connection } = useConnection();
+  const [sol, setSol] = useState();
   const publicKey = wallet?.publicKey?.toString();
   if (!publicKey) {
     return <p>loading.........</p>;
   }
+
+  const handleAirdrop = async () => {
+    if (!sol || isNaN(sol)) {
+      alert("Please enter a valid amount of SOL.");
+      return;
+    }
+
+    const lamports = parseFloat(sol) * 1_000_000_000;
+    try {
+      const signature = await connection.requestAirdrop(
+        wallet.publicKey,
+        lamports
+      );
+      console.log(`Airdrop successful: ${signature}`);
+    } catch (error) {
+      console.error("Airdrop failed:", error);
+    }
+  };
+
   return (
     <div className="relative top-10 text-black flex justify-center">
       <h1>Hi Your public key is {publicKey}</h1>
-      <input className="bg-slate-800" type="text" placeholder="amount" />
-      <button className="bg-slate-400 text-black rounded-2xl w-32">
+      <input
+        onChange={(e) => {
+          setSol(e.target.value.trim());
+        }}
+        className="bg-slate-800"
+        type="text"
+        placeholder="amount"
+      />
+      <button
+        onClick={handleAirdrop}
+        className="bg-slate-400 text-black rounded-2xl w-32"
+      >
         Send airdrop
       </button>
     </div>
