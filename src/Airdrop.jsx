bca74aad97dd6f9bf9a03ef5884bd117f6d54155
Avashneupane9857 <juisch20220011252@gmail.com>
2025-01-07 21:57:09 +0530

    tailwinds config

---
+++ src/Airdrop.jsx
@@ -0,0 +1,8 @@
+export function Airdrop() {
+  return (
+    <div className="relative top-10">
+      <input type="text" placeholder="amount" />
+      <button>Send airdrop</button>
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import  { FC, useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+
 import {
+  WalletConnectButton,
   WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
+// import { clusterApiUrl } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { Airdrop } from "./Airdrop";
 
 function App() {
   return (
@@ -21,8 +19,10 @@ function App() {
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
           <div>
-            <h1>Hi there building a dApp</h1>
+            <h1>Hi this is a dApp</h1>
           </div>
+          <WalletConnectButton></WalletConnectButton>
+          <Airdrop />
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
